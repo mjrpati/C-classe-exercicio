37585ec91cf29922127a10009ba6f0634b9d76d5
Patricia~ <Your Email>
2023-09-01 01:57:45 -0300

    first commit

---
+++ Dados.cs
@@ -0,0 +1,9 @@
+
+namespace aula
+{
+    public class Dados
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Dados pessoa1 = new Dados();
+            Console.WriteLine("Dados da primeira pessoa");
+            Console.WriteLine("Nome:");
+            pessoa1.Nome = Console.ReadLine();
+            Console.WriteLine("Idade:");
+            pessoa1.Idade = int.Parse(Console.ReadLine());
+
+            Dados pessoa2 = new Dados();
+            Console.WriteLine("Dados da segunda pessoa");
+            Console.WriteLine("Nome:");
+            pessoa2.Nome = Console.ReadLine();
+            Console.WriteLine("Idade:");
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+
+            if (pessoa1.Idade > pessoa2.Idade){
+              Console.WriteLine("Pessoa com maior idade é: " + pessoa1.Nome);
+            }
+            else
+            {
+              Console.WriteLine("Pessoa com maior idade é: " + pessoa2.Nome);
+            }
+        }
+
+    }
+}
